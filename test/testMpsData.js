ebf5340b7d8eb56c4b9f826e30ceb04c9ec8425e
d6e <github@d6e.io>
2023-05-26 22:07:59 -0700

    Add a quick smoke test for the mps-data.json

---
+++ test/testMpsData.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const chai = require('chai')
+const expect = chai.expect
+
+// eslint-disable-next-line no-undef
+describe('JSON structure test', function () {
+  // eslint-disable-next-line no-undef
+  it('should have the expected keys and nested objects', function (done) {
+    fs.readFile('./mps-data.json', 'utf8', function (err, data) {
+      if (err) {
+        return done(err)
+      }
+
+      let json
+      try {
+        json = JSON.parse(data)
+      } catch (err) {
+        return done(err)
+      }
+
+      const expectedNestedKeys = ['phenotype', 'broken_geno', 'gene']
+
+      Object.keys(json).forEach(function (key) {
+        // Check the keys of each nested object
+        const nestedObj = json[key]
+        Object.keys(nestedObj).forEach(function (nestedKey) {
+          expect(expectedNestedKeys).to.include(nestedKey)
+        })
+      })
+
+      done()
+    })
+  })
+})
