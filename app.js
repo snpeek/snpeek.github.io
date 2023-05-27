1bb7821f257de678c241f2d1f5b17cbd0ba0051c
d6e <github@d6e.io>
2023-05-26 22:07:59 -0700

    Refactor

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const resultsDiv = document.getElementById('results');
 const progressBar = document.getElementById('progress-bar');
 const progressContainer = document.getElementById('progress-container');
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 
 analyzeBtn.addEventListener('click', () => {
   const file = fileInput.files[0];
@@ -13,7 +21,18 @@ analyzeBtn.addEventListener('click', () => {
     return;
   }
 
-  const snpsToSearch = snpsInput.value.split(',').map(snp => snp.trim());
+  if (file.name.split('.').pop() !== 'txt') {
+    alert('Please upload a .txt file!');
+    return;
+  }
+
+  let snpsToSearch = snpsInput.value.split(',').map(snp => snp.trim());
+  snpsToSearch = snpsToSearch.filter(snp => snp !== '');
+
+  if (snpsToSearch.length === 0) {
+    alert('Please enter at least one SNP!');
+    return;
+  }
 
   progressContainer.style.display = 'block';
   progressBar.style.width = '0%';
@@ -34,8 +53,19 @@ analyzeBtn.addEventListener('click', () => {
       complete: function(results) {
         analyze23AndMeData(results.data, snpsToSearch);
         progressContainer.style.display = 'none';
+      },
+      error: function(error) {
+        console.error("Error while parsing file:", error);
+        alert("An error occurred while parsing the file.");
+        progressContainer.style.display = 'none';
       }
     });
+  };
+
+  reader.onerror = function() {
+    console.error("Error while reading file:", reader.error);
+    alert("An error occurred while reading the file.");
+    progressContainer.style.display = 'none';
   };
 
   reader.readAsText(file);
@@ -43,19 +73,29 @@ analyzeBtn.addEventListener('click', () => {
 
 function analyze23AndMeData(data, snpsToSearch) {
   const foundSnps = {};
+  const resultsFragment = document.createDocumentFragment();
 
   data.forEach(row => {
     if (!row || row.length < 2 || (typeof row[0] === 'string' && row[0].startsWith('#'))) {
-      return; // skip if row is null, undefined, doesn't have enough columns or is a comment
+      return;
     }
-    const snp = row[0];  // Assuming the SNP ID is in the 1st column for a 23andMe file.
+    const snp = row[0];
     if (snpsToSearch.includes(snp)) {
       foundSnps[snp] = true;
     }
   });
 
-  resultsDiv.innerHTML = 'Found SNPs:<br>';
+  const heading = document.createElement('p');
+  heading.textContent = 'Found SNPs:';
+  resultsFragment.appendChild(heading);
+
   for (const snp in foundSnps) {
-    resultsDiv.innerHTML += snp + '<br>';
+    const snpTextNode = document.createTextNode(escapeHtml(snp));
+    const br = document.createElement('br');
+    resultsFragment.appendChild(snpTextNode);
+    resultsFragment.appendChild(br);
   }
+
+  resultsDiv.innerHTML = '';
+  resultsDiv.appendChild(resultsFragment);
 }
