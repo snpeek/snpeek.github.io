6370182f374492997f4905b1e52dff3591ca3e89
Liv Tan Ong <livtanong@gmail.com>
2026-05-02 15:12:16 +0800

    feat: tighter layout for smaller screens

--- src/routes/meyer-powers/+page.svelte
+++ src/routes/meyer-powers/+page.svelte
@@ -23,7 +23,7 @@
     "Estrogen Metabolism",
     "Estrogen Metabolism - 1B Dominant",
     "COMT Activity",
-    "Estrogen Receptor Alpha"
+    "Estrogen Receptor Alpha",
   ];
 
   function onFileInput(event: Event): void {
@@ -78,7 +78,7 @@
 </script>
 
 <main class="flex flex-col gap-8 mb-6">
-  <section class="container mx-auto">
+  <section class="container px-4 md:px-8">
     <h1 class="text-4xl font-bold my-6">Meyer-Powers Syndrome</h1>
     <Alert.Root>
       <Info class="h-4 w-4" />
@@ -131,7 +131,7 @@
       </Alert.Description>
     </Alert.Root>
   </section>
-  <section class="container mx-auto">
+  <section class="container px-4 md:px-8">
     <Input
       class="border-primary"
       id="gene-file"
@@ -140,11 +140,11 @@
     />
   </section>
   {#if files.length > 0 && parseProgress < 100}
-    <section class="container mx-auto">
+    <section class="container px-4 md:px-8">
       <Progress value={parseProgress} />
     </section>
   {/if}
-  <section class="container mx-auto">
+  <section class="container px-4 md:px-8">
     {#if phenotypeSections != undefined}
       {#each phenotypeSections as phenotypeSection}
         <GeneVariantDataTable
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,6 @@ const config: Config = {
 	theme: {
 		container: {
 			center: true,
-			padding: "2rem",
 			screens: {
 				"2xl": "1400px"
 			}
