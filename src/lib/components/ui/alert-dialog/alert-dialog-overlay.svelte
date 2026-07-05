9733ed744574e0fea5a9538afa785710cd33f4f3
Liv Tan Ong <livtanong@gmail.com>
2026-07-05 21:48:30 +0800

    refactor!: fix tailwind classes in some components

--- src/lib/components/ui/alert-dialog/alert-dialog-overlay.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-overlay.svelte
@@ -21,6 +21,6 @@
 <AlertDialogPrimitive.Overlay
 	{transition}
 	{transitionConfig}
-	class={cn("bg-background/80 fixed inset-0 z-50 backdrop-blur-sm ", className)}
+	class={cn("bg-background/80 fixed inset-0 z-50 backdrop-blur-xs ", className)}
 	{...rest}
 />
