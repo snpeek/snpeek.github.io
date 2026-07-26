75f8dfb1b6a701eca12f79672f33e834abfd0ea0
Liv Tan Ong <livtanong@gmail.com>
2026-07-26 10:55:06 +0800

    feat: add dialog component

---
+++ src/lib/components/ui/dialog/dialog-portal.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { Dialog as DialogPrimitive } from "bits-ui";
+  let { ...restProps }: DialogPrimitive.PortalProps = $props();
+</script>
+
+<DialogPrimitive.Portal {...restProps} />
---
+++ src/lib/components/ui/dialog/dialog-title.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Dialog as DialogPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils.js";
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: DialogPrimitive.TitleProps = $props();
+</script>
+
+<DialogPrimitive.Title
+  bind:ref
+  data-slot="dialog-title"
+  class={cn("cn-font-heading leading-none font-medium", className)}
+  {...restProps}
+/>
